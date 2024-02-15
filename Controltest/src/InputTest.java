e11d27308c53dbca5827c53355b66aa52e48a471
Kang Soyeong <a44895575@gmail.com>
2024-02-15 16:35:03 +0900

    0215eclipse

---
+++ Controltest/src/InputTest.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class InputTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO 키보드로부터 입력 받기
+		System.out.print("입력 : ");
+		char input = (char)System.in.read();
+		System.out.println("입력 받은 값은 " + input + "입니다.");
+		
+		//엔터키를 건너뛰게 해주는 메서드
+		System.in.skip(2);
+		
+		System.out.print("숫자입력 : ");
+		int num = System.in.read();
+		System.out.println("입력 받은 값은 " + (num-48) + "입니다.");
+
+
+
+	}
+
+}
