ffa43480b84f2a7931fc3aeb57efbca15a68c026
Kang Soyeong <a44895575@gmail.com>
2024-02-23 09:54:10 +0900

    0223

---
+++ InterfaceTest/src/InterfaceTest1.java
@@ -0,0 +1,15 @@
+//인터페이스도 클래스라 원래는 따로 만들어야함.
+interface Inter1{
+	int a = 10;
+	
+}
+
+public class InterfaceTest1 {
+
+	public static void main(String[] args) {
+		// TODO 인터페이스 기본문법
+		
+
+	}
+
+}
