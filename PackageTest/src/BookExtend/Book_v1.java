2e1bafc12f31a4db950ae0e5a583fb1e8cb7d447
Kang Soyeong <a44895575@gmail.com>
2024-02-26 12:11:39 +0900

    0226

---
+++ PackageTest/src/BookExtend/Book_v1.java
@@ -0,0 +1,24 @@
+package BookExtend;
+import BookPack.Book;
+//클래스 패스 걸려있어서 아무데서나 사용가능
+
+
+public class Book_v1 extends Book {
+private String pubName;
+	
+	public Book_v1(String t, String a, int y, String p){
+		//다른 곳에서 이 패키지의 이 클래스를 사용하려면 pulbic으로 바꿔줘야함.
+
+		super.title = t;
+		super.author = a;
+		super.year = y;
+
+		this.pubName = p;
+	}	
+	
+	
+	public void display(){
+		System.out.println(title + "," + author + "," + year + "," + pubName);
+	}
+
+}
--- PackageTest/src/BookManager.java
+++ PackageTest/src/BookManager.java
@@ -1,3 +1,6 @@
+import BookPack.Book;
+//패키지 불러왔고, 클래스 패스 만들어서 파일위치와상관없이 사용가능.
+import BookExtend.Book_v1;
 
 public class BookManager {
 
@@ -14,6 +17,17 @@ public class BookManager {
 			books[i].display();
 		
 
+		}
+		
+		Book_v1[] books1 = new Book_v1[4];
+		books1[0] = new Book_v1("Java", "Tom", 2005, "A");
+		books1[1] = new Book_v1("Jsp", "Jane", 2007, "A");
+		books1[2] = new Book_v1("Spring", "John", 2000, "B");
+		books1[3] = new Book_v1("React", "Marry", 2010, "C");
+		//다른 패키지에 있으니까 사용하려면 Book_v1을 public으로 만들어야함.
+		
+		for(int i=0; i<books1.length; i++){
+			books1[i].display();
 		}
 
 	}	
